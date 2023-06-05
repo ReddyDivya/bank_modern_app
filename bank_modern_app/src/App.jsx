be621586c929e56b727b3114d83b9e6695d39d19
Divya <divyareddy0205@gmail.com>
2023-06-05 15:54:07 -0700

    App

--- bank_modern_app/src/App.jsx
+++ bank_modern_app/src/App.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
 
 const App = () => (
+  /*
+  overflow-hidden - hides any content that exceeds the boundaries of the <div>.
+  
+  */
   <div className="bg-primary w-full overflow-hidden">
       <h1>Hello, world!</h1>
   </div>
