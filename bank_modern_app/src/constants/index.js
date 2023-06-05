cfb1a5c93e2c4750756e05ac805937529f278543
Divya <divyareddy0205@gmail.com>
2023-06-05 14:59:13 -0700

    index.css

---
+++ bank_modern_app/src/constants/index.js
@@ -0,0 +1,21 @@
+import {people01, people02, people03, facebook, instagram, linkedin, twitter, airbnb,  binance, coinbase, dropbox, send, shield, star} from '../assets';
+
+export const navLink = [
+    {
+        id:'home',
+        title: "Home",
+    },
+    {
+        id:'features',
+        title: "Features",
+    },
+    {
+        id:'product',
+        title: "Product",
+    },
+    {
+        id:'clients',
+        title: "Clients",
+    },
+];
+
