fb52199c8ba1e8c4c886460dc208cc7a1964cfbf
Divya <divyareddy0205@gmail.com>
2023-06-05 15:26:46 -0700

    tailwindcss

--- bank_modern_app/tailwind.config.js
+++ bank_modern_app/tailwind.config.js
@@ -2,19 +2,28 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,jsx}"],
   
-  //allows us to extend or customize the default theme provided by Tailwind CSS
+  //This property allows us to extend or customize the default theme.
   theme: {
     extend: {
+
+      //This extension adds custom colors to the theme.
       colors:{
         primary: "#00040f",
         secondary:"#00f6ff",
         dimWhite: "rgba(255, 255, 255, 0.7)",
         dimBlue: "rgba(9, 151, 124, 0.1)",
       },
+
+      //This extension adds a custom font family named poppins to the theme.
       fontFamily: {
         poppins: ["Poppins", "sans-serif"],
       },
     },
+
+    /*
+    screens property defines custom screen breakpoints for responsive design.
+    breakpoints for various screen sizes, such as xs, ss, sm, md, lg, and xl, each with their respective width values
+    */
     screens:{
       xs:"480px",
       ss:"620px",
