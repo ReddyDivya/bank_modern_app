0c49eeb74add5f60b6d6e5166ac637e83dd2895a
Divya <divyareddy0205@gmail.com>
2023-06-05 16:57:25 -0700

    components

--- bank_modern_app/src/App.jsx
+++ bank_modern_app/src/App.jsx
@@ -1,12 +1,23 @@
-import React from 'react'
+import React from 'react';
+import {Billing, Business, CardDeal, Clients, CTA, Footer, Navbar, Stats, Testimonials, Hero } from "./components";
 
 const App = () => (
   /*
   overflow-hidden - hides any content that exceeds the boundaries of the <div>.
-  
+
   */
   <div className="bg-primary w-full overflow-hidden">
       <h1>Hello, world!</h1>
+      <Navbar />
+      <Hero />
+      <Stats />
+      <Business />
+      <Billing />
+      <CardDeal />
+      <Testimonials />
+      <Clients />
+      <CTA />
+      <Footer />
   </div>
 )
 
