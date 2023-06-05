9ae866a7f7fe46aee49a7e56121bf0b71302a670
Divya <divyareddy0205@gmail.com>
2023-06-05 16:03:56 -0700

    App

---
+++ bank_modern_app/src/style.js
@@ -0,0 +1,8 @@
+const styles = {
+    /*
+    xl:max-w-[1280px] w-full => suggests that the box width should be set to the maximum width of 1280px for extra-large screens.
+    */
+    boxWidth: "xl:max-w-[1280px] w-full",
+
+    heading2: "font-poppins font-semibold xs:text-[48px]"
+};
--- bank_modern_app/src/style.jsx
+++
@@ -1,5 +0,0 @@
-const styles = {
-    boxWidth: "xl:max-w-[1280px] w-full",
-
-    heading2: "font-poppins font-semibold xs:text-[48px] "
-};
