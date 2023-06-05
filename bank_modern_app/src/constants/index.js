7a6a7e51c9f675d1366140188d9e0ad42fe2c5e7
Divya <divyareddy0205@gmail.com>
2023-06-05 15:05:43 -0700

    index.js

--- bank_modern_app/src/constants/index.js
+++ bank_modern_app/src/constants/index.js
@@ -62,4 +62,23 @@ export const feedback = [
         title: "Founder & Leader",
         img: people03,
     },
-]
+];
+
+export const stats = [
+    {
+      id: "stats-1",
+      title: "User Active",
+      value: "3800+",
+    },
+    {
+      id: "stats-2",
+      title: "Trusted by Company",
+      value: "230+",
+    },
+    {
+      id: "stats-3",
+      title: "Transaction",
+      value: "$230M+",
+    },
+];
+
