5de3574a3e8062f1cffe421ec427dc8868cfe209
Divya <divyareddy0205@gmail.com>
2023-06-06 14:58:24 -0700

    Hero component

--- bank_modern_app/src/App.jsx
+++ bank_modern_app/src/App.jsx
@@ -6,6 +6,20 @@ const App = () => (
   /*
   overflow-hidden - hides any content that exceeds the boundaries of the <div>.
   */
+
+  /*
+  Displays:
+  ----------------------
+  1. Navbar Component
+  ---------------
+  Row1 => "HooBank" logo on left and NavLinks on right side of the screen.
+  
+  2. Hero Component
+  ---------------
+  Row2 => "20% Discount For 1 Month Account"
+  Row3 => "The Next Generation" on left and "Get + arrow-up + Started" in middle and Robot image on right side of the screen.
+
+  */
   <div className="bg-primary w-full overflow-hidden">
       <div className={`${styles.paddingX} ${styles.flexCenter}`}>
         <div className={`${styles.boxWidth}`}>
@@ -13,11 +27,14 @@ const App = () => (
         </div>
       </div>
 
+      
       <div className={`bg-primary ${styles.flexStart}`}>
         <div className={`${styles.boxWidth}`}>
           <Hero />
         </div>
       </div>
+
+
       <Stats />
       <Business />
       <Billing />
