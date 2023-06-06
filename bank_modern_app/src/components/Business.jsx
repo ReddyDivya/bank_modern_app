64bfcbcf59a53d5fc953d1f6265b98a49feee619
Divya <divyareddy0205@gmail.com>
2023-06-06 15:52:38 -0700

    business

--- bank_modern_app/src/components/Business.jsx
+++ bank_modern_app/src/components/Business.jsx
@@ -1,6 +1,6 @@
-import React from 'react'
+import { features } from "../constants";
+import styles, { layout } from "../style";
 import Button from "./Button";
-import styles, {layout} from "../style";
 
 /*
 Displays:FeatureCard
@@ -31,28 +31,33 @@ Row1 => "You do the business,
 Row2 => "With the right credit card, you can improve your financial life by building credit, earning rewards and saving money. But with hundreds of credit cards on the market." on left side of the screen.
 Row3 => "Get Started" Button
 */
-const Business = () => (
+const Business = () =>  (
   <section id="features" className={layout.section}>
-      <div className={layout.sectionInfo}>
-          <h2 className={styles.heading2}>
-              You do the business, <br className="sm:block hidden" /> we’ll handle
-            the money.
-          </h2>
-          <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
-            With the right credit card, you can improve your financial life by
-            building credit, earning rewards and saving money. But with hundreds
-            of credit cards on the market.
-          </p>
+    <div className={layout.sectionInfo}>
+      <h2 className={styles.heading2}>
+        You do the business, <br className="sm:block hidden" /> we’ll handle
+        the money.
+      </h2>
+      <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+        With the right credit card, you can improve your financial life by
+        building credit, earning rewards and saving money. But with hundreds
+        of credit cards on the market.
+      </p>
 
-          <Button styles={`mt-10`} />
-      </div>
+      <Button styles={`mt-10`} />
+    </div>
 
-      {/*Feature Card*/}
-      <div className={`${layout.sectionImg} flex-col`}>
-        {features.map((feature, index) => (
-          <FeatureCard key={feature.id} {...feature} index={index} />
-        ))}
-      </div>
+    <div className={`${layout.sectionImg} flex-col`}>
+      {features.map((feature, index) => (
+        /*
+        ...feature: This spread operator (...) spreads all the properties of the feature object as individual props to the FeatureCard component. 
+        feature object => id, icon, title, content
+        It means that the feature object has several properties, and each property will be passed as a separate prop to the FeatureCard component.
+        index={index}: It passes the index variable as a prop named index to the FeatureCard component.
+        */
+        <FeatureCard key={feature.id} {...feature} index={index} />
+      ))}
+    </div>
   </section>
 );//Business
 
