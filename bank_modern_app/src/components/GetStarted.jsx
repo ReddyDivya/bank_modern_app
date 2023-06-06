c8429e47f7773428ad3227da963efb2d92a6ce7a
Divya <divyareddy0205@gmail.com>
2023-06-06 12:18:02 -0700

    Hero component

--- bank_modern_app/src/components/GetStarted.jsx
+++ bank_modern_app/src/components/GetStarted.jsx
@@ -5,15 +5,27 @@ import {arrowUp} from "../assets";
 const GetStarted = () => (
   /*
   bg-blue-gradient => customized class, check index.css
+  text-gradient => customized class, check index.css 
   */
 
+  /*displays "Get + arrow-up" in a circle*/
   <div className={`${styles.flexCenter} w-[140px] h-[140px] rounded-full bg-blue-gradient p-[2px] cursor-pointer`}>
+      
+      {/*display "Get + arrow-up" in a circle*/}
       <div className={`${styles.flexCenter} flex-col bg-primary w-[100%] h-[100%] rounded-full`}>
+
+          {/*display "Get + arrow-up" text*/}
           <div className={`${styles.flexStart} flex-row`}>
 
+              {/*display "Get" text*/}
+              <p className="font-poppins font-medium text-[18px] leading-[23.4px]">
+                  <span className="text-gradient">Get</span>
+              </p>
+
+              {/*display arrow icon*/}
+              <img src={arrowUp} alt="arrow-up" className="w-[23px] h-[23px] object-contain"/>
           </div>
       </div>
-
   </div>
 );
 export default GetStarted
--- bank_modern_app/src/components/Hero.jsx
+++ bank_modern_app/src/components/Hero.jsx
@@ -14,22 +14,25 @@ const Hero = () => {
             
             {/* 1. displays discount information*/}
             <div className="flex flex-row items-center py-[6px] px-4 bg-discount-gradient rounded-[10px] mb-2">
-
-                {/* displays discount image*/}
+                {/*1a. displays discount image*/}
                 <img src={discount} alt="discount" className="w-[32px] h-[32px]"/>
                 
-                {/* displays discount info*/}
+                {/*1b. displays discount info*/}
                 <p className={`${styles.paragraph} ml-2`}>
                   <span className="text-white">20%</span> Discount For{" "}
                   <span className="text-white">1 Month</span> Account
                 </p>
             </div>
 
+            {/*2. displays "The Next Generation" on left side and "Get + arrow-up" in a circle on the right side of the screen.*/}
             <div className="flex flex-row justify-between items-center w-full">
+                {/*2a. displays "The Next Generation on left side of the screen*/}
                 <h1 className="flex-1 font-poppins font-semibold ss:text-[72px] text-[52px] text-white ss:leading-[100.8px] leading-[75px]">
                     The Next <br className="sm:block hidden"/>{" "}
                     <span className="text-gradient">Generation</span>{" "}
                 </h1>
+
+                {/*2b. displays "Get + arrow-up" in a circle on right side of the screen*/}
                 <div className="ss:flex hidden md:mr-4 mr-0">
                     <GetStarted/>
                 </div>
