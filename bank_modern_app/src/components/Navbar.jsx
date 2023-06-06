5d54017ed3331e4e263859198ae45c2a48d871a9
Divya <divyareddy0205@gmail.com>
2023-06-05 17:05:28 -0700

    navbar

--- bank_modern_app/src/components/Navbar.jsx
+++ bank_modern_app/src/components/Navbar.jsx
@@ -1,7 +1,12 @@
 import React, {useState} from 'react'
 import {close, logo, menu} from '../assets';
+import {navLinks} from "../constants";//get navLinks
 
 const Navbar = () => {
+
+  const [active, setActive] = useState("Home");//default "Home" is active
+  const [toggle, setToggle] = useState(false);//for toggling
+
   return (
     <div>Navbar</div>
   )
