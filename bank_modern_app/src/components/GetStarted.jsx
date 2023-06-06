8c7dde4b26a6fe86d38b27f407b165b85a557fdc
Divya <divyareddy0205@gmail.com>
2023-06-06 12:22:34 -0700

    Hero component

--- bank_modern_app/src/components/GetStarted.jsx
+++ bank_modern_app/src/components/GetStarted.jsx
@@ -8,21 +8,18 @@ const GetStarted = () => (
   text-gradient => customized class, check index.css 
   */
 
-  /*displays "Get + arrow-up" in a circle*/
+  /*1. displays "Get + arrow-up" in a circle*/
   <div className={`${styles.flexCenter} w-[140px] h-[140px] rounded-full bg-blue-gradient p-[2px] cursor-pointer`}>
-      
-      {/*display "Get + arrow-up" in a circle*/}
+      {/*displays circle with only border*/}
       <div className={`${styles.flexCenter} flex-col bg-primary w-[100%] h-[100%] rounded-full`}>
-
           {/*display "Get + arrow-up" text*/}
           <div className={`${styles.flexStart} flex-row`}>
-
-              {/*display "Get" text*/}
+              {/*1a. display "Get" text*/}
               <p className="font-poppins font-medium text-[18px] leading-[23.4px]">
                   <span className="text-gradient">Get</span>
               </p>
 
-              {/*display arrow icon*/}
+              {/*1b. display arrow icon*/}
               <img src={arrowUp} alt="arrow-up" className="w-[23px] h-[23px] object-contain"/>
           </div>
       </div>
