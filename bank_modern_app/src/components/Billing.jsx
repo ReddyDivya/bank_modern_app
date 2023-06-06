3d1c6aac0fe0355c505c251fb78b362fa34891be
Divya <divyareddy0205@gmail.com>
2023-06-06 16:41:02 -0700

    billing

--- bank_modern_app/src/components/Billing.jsx
+++ bank_modern_app/src/components/Billing.jsx
@@ -2,10 +2,17 @@ import React from 'react';
 import { apple, bill, google } from "../assets";
 import styles, { layout } from "../style";
 
+/*
+Displays:Billing
+-----------------------------------------
+Row1 => 
+- displays bill image & gradient on left side of the screen.
+- displays billing heading and description on right side of the screen.
+*/
 const Billing = () => (
   <section id="product" className={layout.sectionReverse}>
       
-      {/*displays bill image & gradient*/}
+      {/*1. displays bill image & gradient on left side of the screen*/}
       <div className={layout.sectionImgReverse}>    
           {/*displays bill image*/}
           <img src={bill} alt="billing"  className="w-[100%] h-[100%] relative z-[5]"/>
@@ -15,12 +22,20 @@ const Billing = () => (
           <div className="absolute z-[0] -left-1/2 bottom-0 w-[50%] h-[50%] rounded-full pink__gradient"/>
       </div>
 
-      {/**/}
+      {/*2. displays billing heading and description on right side of the screen*/}
       <div className={layout.sectionInfo}>
+          {/*displays billing heading on right side of the screen*/}
           <h2 className={styles.heading2}>
           Easily control your <br className="sm:block hidden" /> billing &
           invoicing
           </h2>
+
+          {/*displays billing description on right side of the screen*/}
+          <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+            Elit enim sed massa etiam. Mauris eu adipiscing ultrices ametodio
+            aenean neque. Fusce ipsum orci rhoncus aliporttitor integer platea
+            placerat.
+          </p>
       </div>
 
 
