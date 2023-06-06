4263c047033fb260aa381eb0363ced76559f5427
Divya <divyareddy0205@gmail.com>
2023-06-05 17:31:11 -0700

    Navbar

--- bank_modern_app/src/components/Navbar.jsx
+++ bank_modern_app/src/components/Navbar.jsx
@@ -29,6 +29,14 @@ const Navbar = () => {
             ))
           }
         </ul>
+
+        <div className="sm:hidden flex flex-1 justify-end items-center">
+          <img src={toggle ? close : menu}
+          alt='menu'
+          className="w-[28px] h-[28px] object-contain"
+          onClick={() => setToggle(!toggle)}
+          />
+        </div>
     </nav>
   )
 }
