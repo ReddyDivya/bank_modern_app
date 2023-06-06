f0563f5a970fa0602440c9fd002010b9394fd8a9
Divya <divyareddy0205@gmail.com>
2023-06-06 13:08:22 -0700

    Hero component

--- bank_modern_app/src/components/Hero.jsx
+++ bank_modern_app/src/components/Hero.jsx
@@ -60,6 +60,23 @@ const Hero = () => {
         <div className={`flex-1 flex ${styles.flexCenter} md:my-0 my-10 relative`}>
             {/*3a. displays robot image on right side of the screen.*/}
             <img src={robot} alt="billing" className="w-[100%] h-[100%] relative z-[5]" />
+
+            {/*3b. gradient start */}
+            <div className="absolute z-[0] w-[40%] h-[35%] top-0 pink__gradient"/>
+            <div className="absolute z-[1] w-[80%] h-[80%] rounded-full white__gradient bottom-40"/>
+            <div className="absolute z-[0] w-[50%] h-[50%] right-20 bottom-20 blue__gradient"/>
+            {/*3b. gradient end */}
+        </div>
+
+        {
+          /* 
+          small screen 
+          ---------------------
+          2b. displays "Get + arrow-up" in a circle on small screen
+          */
+        }
+        <div className={`ss:hidden ${styles.flexCenter}`}>
+          <GetStarted />
         </div>
     </section>
   )
