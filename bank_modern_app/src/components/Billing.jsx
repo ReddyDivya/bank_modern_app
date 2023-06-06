58b3aa2d798bdb8c56b7a919a35874e67e55a175
Divya <divyareddy0205@gmail.com>
2023-06-06 16:25:12 -0700

    billing

--- bank_modern_app/src/components/Billing.jsx
+++ bank_modern_app/src/components/Billing.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from 'react';
+import { apple, bill, google } from "../assets";
+import styles, { layout } from "../style";
 
-const Billing = () => {
-  return (
-    <div>Billing</div>
-  )
-}
+const Billing = () => (
+  <section id="product" className={layout.sectionReverse}>
+
+  </section>  
+)
 
 export default Billing
