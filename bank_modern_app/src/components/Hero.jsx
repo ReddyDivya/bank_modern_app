f2dfde151d1c5f34d286e8f63ce877fd76be801a
Divya <divyareddy0205@gmail.com>
2023-06-05 17:48:42 -0700

    Hero

--- bank_modern_app/src/components/Hero.jsx
+++ bank_modern_app/src/components/Hero.jsx
@@ -1,8 +1,13 @@
-import React from 'react'
+import React from 'react';
+import styles from "../style";
+import {discount, robot} from "../assets";
+import GetStarted from './GetStarted';
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <section id="home" className={`flex md:flex-row flex-col ${styles.paddingY}`}>
+        
+    </section>
   )
 }
 
