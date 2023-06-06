2558b39e808a0ba32857ade29bb2c6416f6d86ad
Divya <divyareddy0205@gmail.com>
2023-06-06 13:01:35 -0700

    Hero component

--- bank_modern_app/src/components/Hero.jsx
+++ bank_modern_app/src/components/Hero.jsx
@@ -42,6 +42,24 @@ const Hero = () => {
                     <GetStarted/>
                 </div>
             </div>
+
+            {/*displays "Payment Method" on left side of the screen.*/}
+            <h1 className="font-poppins font-semibold ss:text-[68px] text-[52px] text-white ss:leading-[100.8px] leading-[75px] w-full">
+                Payment Method
+            </h1>
+
+            {/*displays some text on left side of the screen.*/}
+            <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+                Our team of experts uses a methodology to identify the credit cards
+                most likely to fit your needs. We examine annual percentage rates,
+                annual fees.
+            </p>
+        </div>
+
+        {/*3. displays robot image on right side of the screen.*/}
+        <div className={`flex-1 flex ${styles.flexCenter} md:my-0 my-10 relative`}>
+            {/*3a. displays robot image on right side of the screen.*/}
+            <img src={robot} alt="billing" className="w-[100%] h-[100%] relative z-[5]" />
         </div>
     </section>
   )
